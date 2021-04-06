d163dc467667e15b8cae585f01f4f6beff540817
Jorge Perez <jperezpenaranda@gmail.com>
2021-04-06 11:44:24 -0500

    responsive

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,20 +12,20 @@ export const Footer = () => {
             <div className="footer-contact">
                 <h3>Contact me</h3>
                 <p>And let´s get down to work</p>
-                <p ><i class="fas fa-envelope"></i> JPEREZPENARANDA@GMAIL.COM</p>
+                <p ><i class="fas fa-envelope"></i> jperezpenaranda@gmail.com</p>
             </div>
             <div className="footer-sns">
                 <div className="design-by">
                     Design by  <p className="footer-nombre">Shunnior.</p>
                 </div>
                 <div className="sns-links">
-                    <a href="https://www.linkedin.com/in/jorge-omar-p%C3%A9rez-pe%C3%B1aranda-a8b20a188/" target="_blank" rel="noreferrer">
+                    <a className="social" href="https://www.linkedin.com/in/jorge-omar-p%C3%A9rez-pe%C3%B1aranda-a8b20a188/" target="_blank" rel="noreferrer">
                         <i className=" fab fa-linkedin linkedin"></i>                       
                     </a>
-                    <a href="https://github.com/SHUNNIORR" target="_blank" rel="noreferrer">
+                    <a className="social" href="https://github.com/SHUNNIORR" target="_blank" rel="noreferrer">
                         <i className=" fab fa-github github"></i>
                     </a>
-                    <a href="https://www.instagram.com/shunnior/" target="_blank" rel="noreferrer">
+                    <a className="social" href="https://www.instagram.com/shunnior/" target="_blank" rel="noreferrer">
                         <i className=" fab fa-instagram instagram"></i>
                     </a>
                 </div>
